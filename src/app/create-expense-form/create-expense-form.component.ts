be3a0141204a2fb4d5c74443273a9a8f5407f633
“Julio <ju.1996@outlook.com>
2023-11-14 22:39:19 -0300

    Adding improvement on forms

--- src/app/create-expense-form/create-expense-form.component.ts
+++ src/app/create-expense-form/create-expense-form.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Expense } from '../model/expense';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-expense-form',
@@ -8,6 +9,29 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./create-expense-form.component.css']
 })
 export class CreateExpenseFormComponent {
+  @ViewChild('form') form!: NgForm;
+  @ViewChild('categorySelect') categorySelect!: ElementRef;
+
+  categories: string[] = [  
+    'Salary',
+    'Health',
+    'Food',
+    'Hobby',
+    'Bill',
+    'Fuel',
+    'Others'
+  ];
+
+  colorsCategoryMap = new Map<string, string>([
+    [this.categories[0], "light-blue"],
+    [this.categories[1], "light-green accent-3"],
+    [this.categories[2], "yellow accent-2"],
+    [this.categories[3], "cyan accent-2"],
+    [this.categories[4], "red accent-2"],
+    [this.categories[5], "red"],
+    [this.categories[6], "purple accent-1"]
+  ]);
+
   userRandomName: string = "User";
   expense!: Expense;
 
@@ -36,9 +60,13 @@ export class CreateExpenseFormComponent {
 
   }
 
-  onButtonSaveClick() {
+  // Save Button
+  onSubmit() {
     alert(`Saving Expense Name = ${this.expense.name}
-      Saving Expense Value = ${this.expense.value}`);
+      Saving Expense Value = ${this.expense.value}
+      Expense Category = ${this.expense.category}
+      Expense Date = ${this.expense.date}
+      `);
       this.warningMessage = `Expense ${this.expense.name} Saved !`;
       this.warningSubMessage = "Test!"
       this.saveSucceeded = true;
@@ -53,6 +81,12 @@ export class CreateExpenseFormComponent {
       this.warningSubMessage = "Test!"
       this.deleteSucceeded = true;
       this.saveSucceeded = false;
-      this.warningIcon = "report"
+      this.warningIcon = "report";
+  }
+
+
+  ngAfterViewInit() {
+    // Initialize Materialize Select
+    M.FormSelect.init(this.categorySelect.nativeElement);
   }
 }
